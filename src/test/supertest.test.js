dde2c1b54bcd1de8a6ebbe4a7c2b0371ce70d88e
rodrigosdrigotti <rodrigosdrigotti@gmail.com>
2024-05-03 00:06:33 -0300

    Desafio Modulos de Testing

--- src/test/supertest.test.js
+++ src/test/supertest.test.js
@@ -4,9 +4,10 @@ const supertest = require('supertest')
 const expect = chai.expect
 const requester = supertest('http://localhost:8080')
 
-describe("Testing Products", () => {
+describe("Testing Products and Carts", () => {
     let authTokenCookie
     let productID
+    let cartID
     describe('POST /login', () => {
         it('Login', async () => {
             const res = await requester
@@ -17,10 +18,9 @@ describe("Testing Products", () => {
             expect(res.body.status).to.equal('Success')
             expect(res.body.payload).to.equal('Logged In')
             
-            expect(res.header['set-cookie']).to.be.an('array');
-            authTokenCookie = res.header['set-cookie'].find(cookie => cookie.startsWith('authToken='));
-            console.log(authTokenCookie)
-            expect(authTokenCookie).to.exist; 
+            expect(res.header['set-cookie']).to.be.an('array')
+            authTokenCookie = res.header['set-cookie'].find(cookie => cookie.startsWith('authToken='))
+            expect(authTokenCookie).to.exist
         })
     })
 
@@ -93,4 +93,54 @@ describe("Testing Products", () => {
             expect(res.body.status).to.equal('success')
         })
     })
+
+    describe("Test GET /carts", () => {
+        it("Devuelve todos los carritos", async () => {
+            const res = await requester
+                .get('/api/carts')
+                .set('cookie', authTokenCookie)
+            
+            expect(res.ok).to.be.true
+            expect(res.text).to.include('<div class="cart-container">')
+        })
+    })
+
+    describe("Test POST /carts", () => {
+        it("Agrega un Carrito Vacio Sino Suma Cantidad", async () => {
+            const cartMock = {
+                productId: "5f6a3f278d3d9517a0cd1f83",
+                quantity: 1
+            }
+            const userData = {
+                email: 'sabrina.avallone2@gmail.com',
+                role: 'admin'
+            } 
+            const { status, ok, _body } = await requester
+                .post('/api/carts')
+                .set('cookie', authTokenCookie)
+                .set('user', userData)
+                .send(cartMock)
+
+            console.log(status, ok, _body)
+            cartID = _body.payload.newCart._id
+            expect(_body.payload.newCart).to.have.property('_id')
+        })
+    })
+
+    describe("Test DELETE /carts/:cid", () => {
+        it("Elimina un Producto a traves del ID", async () => {
+            const userData = {
+                email: 'sabrina.avallone2@gmail.com',
+                role: 'premium'
+            } 
+            const res = await requester
+                .delete(`/api/carts/${cartID}`)
+                .set('cookie', authTokenCookie)
+                .set('user', userData)
+            
+            expect(res.status).to.equal(201)
+            expect(res.body.status).to.equal('Success')
+        })
+    })
 })
+
