89ba7e520b091275d3764520144d642d79cff62f
rodrigosdrigotti <rodrigosdrigotti@gmail.com>
2024-05-27 23:18:17 -0300

    Entrega Proyecto Final

--- src/adapters/factory.js
+++ src/adapters/factory.js
@@ -2,12 +2,12 @@ const { environment } = require('../configs/server.config')
 
 switch (environment) {
   case 'dev':
-    console.log('Usando Nodemailer')
+    //console.log('Usando Nodemailer')
     module.exports = require('./mail.adapter')
     break
 
   case 'prod':
-    console.log('Usando Twilio')
+    //console.log('Usando Twilio')
     //! FALTARIA USAR SMS DE TWILIO
     module.exports = require('./mail.adapter')
     break
